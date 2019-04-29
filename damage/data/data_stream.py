29aafff4dbe71b20a2d3be8330d39fe9afa971fb
javiermas <javier.mas@barcelonagse.eu>
2019-04-29 15:25:48 +0200

    :sparkles: Create method get_generator in DataStream

--- damage/data/data_stream.py
+++ damage/data/data_stream.py
@@ -1,9 +1,22 @@
+from math import ceil
+
+
 class DataStream:
     def __init__(self, x, y, batch_size):
         self.x = x
         self.y = y
         self.images_per_batch = batch_size
         self.current_batch_num = 0
+        self.num_batches = ceil(self.images_per_batch / batch_size)
+
+    def get_generator(self):
+        while True:
+            for i in range(self.num_batches):
+                batch = (
+                    self.x[(i*self.images_per_batch): ((i+1)*self.images_per_batch)],
+                    self.y[(i*self.images_per_batch): ((i+1)*self.images_per_batch)]
+                )
+                yield batch
 
     def get_batch(self):
         x_batch = self.x[(self.current_batch_num*self.images_per_batch):
