51f634c20743425aba1bc077bd6abee932751abd
javiermas <javier.mas@barcelonagse.eu>
2019-05-03 20:32:07 +0200

    :sparkles: Add validate_generator method to CNN

--- damage/models/cnn.py
+++ damage/models/cnn.py
@@ -1,11 +1,17 @@
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, BatchNormalization
 from tensorflow.keras.models import Sequential
 
+from damage.models.losses import (precision, recall, true_positives, true_negatives,
+                                  false_positives, false_negatives, positives, negatives)
+
 
 class CNN:
-    def __init__(self, convolutional_layers, learning_rate=0.1, num_classes=2, weight_positives=1):
-        self.num_classes = num_classes
+    def __init__(self, convolutional_layers, dense_units=64, learning_rate=0.1,
+                 num_classes=2, weight_positives=1, **kwargs):
         self.convolutional_layers = convolutional_layers
+        self.dense_units = dense_units
+        self.learning_rate = learning_rate
+        self.num_classes = num_classes
         self.model = self._create_model()
 
     def _create_model(self):
@@ -15,22 +21,35 @@ class CNN:
 
         layers.extend([
             Flatten(),
-            Dense(units=64),
+            Dense(units=self.dense_units),
             BatchNormalization(),
             Dense(units=self.num_classes, activation='softmax'),
         ])
         model = Sequential(layers)
-        model.compile(optimizer='adam', loss='binary_crossentropy')
+        model.compile(optimizer='adam', loss='binary_crossentropy', learning_rate=self.learning_rate,
+                      metrics=['accuracy', precision, recall, true_positives, true_negatives,
+                               false_negatives, false_positives, positives, negatives])
         return model
 
-    def fit(self, x, y, epochs):
+    def fit(self, x, y, epochs, **kwargs):
         self.model.fit_generator(x, y, epochs=epochs)
 
-    def fit_generator(self, generator, epochs, steps_per_epoch):
+    def fit_generator(self, generator, epochs, steps_per_epoch, **kwargs):
         self.model.fit_generator(generator, epochs=epochs, steps_per_epoch=steps_per_epoch)
+
+    def validate_generator(self, train_generator, test_generator, validation_steps,
+                           epochs, steps_per_epoch, **kwargs):
+        epochs=2
+        model_fit = self.model.fit_generator(train_generator, validation_data=test_generator,
+                                             epochs=epochs, validation_steps=validation_steps,
+                                             steps_per_epoch=steps_per_epoch)
+        return model_fit.history
+
+    def predict(self, x):
+        return self.model.predict(x)
 
     @staticmethod
     def _create_convolutional_and_pooling_layer(filters, kernel_size, pool_size):
         conv = Conv2D(filters=filters, kernel_size=kernel_size, padding="same", activation='relu')
-        pool = MaxPooling2D(pool_size=pool_size, strides=pool_size[0])
+        pool = MaxPooling2D(pool_size=pool_size, strides=1)
         return pool
