3eea05504ddd875c6f70d9fece2919680660f3fc
javiermas <javier.mas@barcelonagse.eu>
2019-05-03 20:26:32 +0200

    :sparkles: Add RandomSearch class

--- damage/models/__init__.py
+++ damage/models/__init__.py
@@ -1,2 +1,3 @@
 from .image_classification_network import ImageClassificationNetwork
 from .cnn import CNN
+from .random_search import RandomSearch
---
+++ damage/models/random_search.py
@@ -0,0 +1,35 @@
+import random
+import numpy as np
+
+
+class RandomSearch:
+
+    def __init__(self):
+        pass
+
+    def sample_cnn(self, T):
+        spaces = [self._sample_single_cnn_space() for _ in range(T)]
+        return spaces
+    
+    @staticmethod
+    def _sample_single_cnn_space():    
+        num_layers = random.choice(range(1, 20))
+        convolutional_layers = []
+        for _ in range(num_layers):
+            kernel_size = random.choice(range(1, 15))
+            pool_size = min(kernel_size-1, random.choice(range(1, 10)))
+            layer = {
+                'kernel_size': [kernel_size, kernel_size],
+                'pool_size': [pool_size, pool_size],
+                'filters': random.choice(range(10, 300)),
+            }
+            convolutional_layers.append(layer)
+        
+        space = {
+            'learning_rate': random.choice(np.geomspace(1e-2, 1)),
+            'batch_size': random.choice(range(5, 200)),
+            'convolutional_layers': convolutional_layers,
+            'epochs': random.choice(range(20, 200)),
+        }
+        return space
+
