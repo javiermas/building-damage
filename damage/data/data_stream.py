4aeca45918a81f99dc1ef1bb5293fb411a3d366c
javiermas <javier.mas@barcelonagse.eu>
2019-05-03 20:26:02 +0200

    :sparkles: Create method get_generators and rename get_generator to get_single_generator

--- damage/data/data_stream.py
+++ damage/data/data_stream.py
@@ -1,28 +1,39 @@
 from math import ceil
+import random
+import numpy as np
 
 
 class DataStream:
     def __init__(self, x, y, batch_size):
         self.x = x
         self.y = y
-        self.images_per_batch = batch_size
+        self.batch_size = batch_size
         self.current_batch_num = 0
-        self.num_batches = ceil(self.images_per_batch / batch_size)
+        self.num_batches = ceil(len(self.y) / batch_size)
 
-    def get_generator(self):
+    def get_generators(self, test_proportion=.8):
+        train_index = random.sample(range(len(self.y)), round(len(self.y)*0.8))
+        train_generator = self.get_single_generator(self.x[train_index], self.y[train_index], self.batch_size)
+        test_generator = self.get_single_generator(np.delete(self.x, train_index, axis=0),
+                                                   np.delete(self.y, train_index, axis=0),
+                                                   len(self.y) - len(train_index))
+        return train_generator, test_generator
+
+    def get_single_generator(self, x, y, batch_size):
+        num_batches = ceil(len(y) / batch_size)
         while True:
-            for i in range(self.num_batches):
+            for i in range(num_batches):
                 batch = (
-                    self.x[(i*self.images_per_batch): ((i+1)*self.images_per_batch)],
-                    self.y[(i*self.images_per_batch): ((i+1)*self.images_per_batch)]
+                    x[(i*batch_size): ((i+1)*self.batch_size)],
+                    y[(i*batch_size): ((i+1)*self.batch_size)]
                 )
                 yield batch
-
+    
     def get_batch(self):
-        x_batch = self.x[(self.current_batch_num*self.images_per_batch):
-                         ((self.current_batch_num+1)*self.images_per_batch)]
-        y_batch = self.y[(self.current_batch_num*self.images_per_batch):
-                         ((self.current_batch_num+1)*self.images_per_batch)]
+        x_batch = self.x[(self.current_batch_num*self.batch_size):
+                         ((self.current_batch_num+1)*self.batch_size)]
+        y_batch = self.y[(self.current_batch_num*self.batch_size):
+                         ((self.current_batch_num+1)*self.batch_size)]
         return x_batch, y_batch
 
     def update(self):
