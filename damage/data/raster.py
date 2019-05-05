1a9935f634958d713281e4b9d82bdc69a105be82
javiermas <javier.mas@barcelonagse.eu>
2019-05-05 21:31:13 +0200

    :fire: Remove plot and split methods from Raster

--- damage/data/raster.py
+++ damage/data/raster.py
@@ -1,8 +1,5 @@
 import numpy as np
-from matplotlib import pyplot as plt
 import rasterio
-
-from damage.utils import update_progress
 
 
 class Raster:
@@ -36,31 +33,3 @@ class Raster:
         raster_array = self.raster.read(indexes=[1,2,3])
         raster_array = np.swapaxes(np.swapaxes(raster_array, 1, 2), 0, 2)
         return raster_array
-
-    def plot(self):
-        raster_array = self.to_array()
-        _, ax = plt.subplots(figsize=(25, 25))
-        ax.imshow(raster_array)
-
-    def split(self, tile_size=64, stride=16): 
-        array = self.to_array().astype(float)
-        tiles = []
-        number_of_iterations = len(range(tile_size//2, (self.width - tile_size//2), stride))
-        for i, w in enumerate(range(tile_size//2, (self.width - tile_size//2), stride)):
-            for h in range(tile_size//2, (self.height - tile_size//2), stride):
-                longitude, latitude = self.raster.xy(h, w)
-                left = (w - tile_size//2)
-                right = (w + tile_size//2 + 1)
-                top = (h - tile_size//2)
-                bottom = (h + tile_size//2 + 1)
-                tile = {
-                    'image': array[top:bottom, left:right],
-                    'longitude': longitude,
-                    'latitude': latitude,
-                }
-                tiles.append(tile)
-
-            update_progress(i / number_of_iterations)
-
-        update_progress(1)
-        return tiles
