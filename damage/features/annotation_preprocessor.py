491d9c05fba852fe47d46cfdaf13897f49a270d4
javiermas <javier.mas@barcelonagse.eu>
2019-05-06 09:57:01 +0200

    :sparkles: Create class AnnotationPreprocessor

---
+++ damage/features/annotation_preprocessor.py
@@ -0,0 +1,18 @@
+from damage.utils import geo_location_index
+
+class AnnotationPreprocessor:
+
+    def __init__(self, grid_size=0.035):
+        self.grid_size = grid_size
+
+    def transform(self, data):
+        for key, value in data.items():
+            if 'annotation' not in key:
+                continue
+
+            value['latitude'] = value['geometry'].apply(lambda point: point.y)
+            value['longitude'] = value['geometry'].apply(lambda point: point.x)
+            value['location_index'] = geo_location_index(value['latitude'], value['longitude'], self.grid_size)
+            data[key] = value
+            
+        return data
