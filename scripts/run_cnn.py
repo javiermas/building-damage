98b9d12fcb0f745ecc712dc8e9b09eda8cf1fa9f
javiermas <javier.mas@barcelonagse.eu>
2019-05-06 12:26:55 +0200

    :clean: Clean run_cnn script up

--- scripts/run_cnn.py
+++ scripts/run_cnn.py
@@ -1,6 +1,5 @@
 import os
 import geopandas as gpd
-import rasterio
 import numpy as np
 import pandas as pd
 
@@ -9,43 +8,12 @@ from damage.models import CNN, RandomSearch
 from damage import features
 
 
-def preprocess_damage_data(damage_data):
-    damage_data['DmgCls'] = damage_data['DmgCls'].replace({None: 'No damage'})
-    damage_data['geometry_tuple'] = damage_data['geometry'].apply(lambda point: (point.x, point.y))
-    damage_data['latitude'] = damage_data['geometry'].apply(lambda point: point.y)
-    damage_data['longitude'] = damage_data['geometry'].apply(lambda point: point.x)
-    return damage_data
-
-def get_image_index_from_annotation(annotation_data, city_data):
-    return annotation_data.apply(lambda x:
-        pd.Series({key: i for key, i in zip(['row', 'col'], city_data.index(x['longitude'], x['latitude']))}),
-        axis=1)
-
 def crop_annotation_to_image_dimensions(annotation_data, dimensions):
     mask = (annotation_data['row'] < dimensions['height'])\
         & (annotation_data['row'] >= 0)\
         & (annotation_data['col'] < dimensions['width'])\
         & (annotation_data['col'] >= 0)
     return annotation_data.loc[mask]
-
-def raster_to_array(raster_data):
-    raster_array = raster_data.read(indexes=[1,2,3])
-    raster_array = np.swapaxes(np.swapaxes(raster_array, 1, 2), 0, 2)
-    return raster_array
-
-def create_feature_matrix(city_array, annotation_data, window=50):
-    assert (window) % 2 == 0
-    extra_pixels = int((window)/2)
-    feature_data = []
-    for ix, row in annotation_data.iterrows():
-        pixel_row, pixel_col = row['row'], row['col']
-        building = get_building_frame(city_array, pixel_row, pixel_col, extra_pixels)
-        feature_data.append({'image': building, 'target': (row['DmgCls_2'] == 'Destroyed') * 1})
-        
-    return pd.DataFrame(feature_data)
-
-def get_building_frame(city_array, row, col, extra_pixels):
-    return city_array[(row-extra_pixels):(row+extra_pixels), (col-extra_pixels):(col+extra_pixels), :].astype(float)
 
 def read_annotations(file_names):
     path = 'data/annotations/'
@@ -64,11 +32,15 @@ def read_rasters(file_names):
     return data
 ## Reading
 annotation_data = read_annotations(file_names=[
-    '6_Damage_Sites_Aleppo_SDA.shp',
+    '4_Damage_Sites_Raqqa_CDA.shp',
 ])
 raster_data = read_rasters(file_names=[
-    'aleppo_2011_06_26_zoom_19.tif',
-    'aleppo_2016_10_19_zoom_19.tif',
+    'homs_2011_05_21_zoom_19.tif',
+    'homs_2016_05_30_zoom_19.tif',
+    #'raqqa_2013_01_17_zoom_19.tif',
+    #'raqqa_2016_07_01_zoom_19.tif',
+    #'aleppo_2011_06_26_zoom_19.tif',
+    #'aleppo_2016_10_19_zoom_19.tif',
 ])
 data = {**annotation_data, **raster_data}
 ### Processing
