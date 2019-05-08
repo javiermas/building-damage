c8ca4932ad672d7b2d63f44a7d605eeacd354a72
Javier Mas <javier.mas@barcelonagse.eu>
2019-05-08 08:41:32 +0200

    :bug: Fix typo on base

--- damage/features/base.py
+++ damage/features/base.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class Transformer(ABC):
 
     def __call__(self, *args, **kwargs):
-        return self.transform(*args. **kwargs)
+        return self.transform(*args, **kwargs)
 
     @abstractmethod
     def transform(self, data):
