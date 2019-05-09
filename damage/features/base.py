d0f7921b1cc5c9c3240790d824acbbe4d662abc6
javiermas <javier.mas@barcelonagse.eu>
2019-05-09 14:09:07 +0200

    :bulb: Add docstrings to base transformer classes

--- damage/features/base.py
+++ damage/features/base.py
@@ -2,7 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Transformer(ABC):
-
+    """ A Transformer class is a class with a public method transform
+    that takes a dictionary of data and returns some transformation
+    of that data.
+    """
     def __call__(self, *args, **kwargs):
         return self.transform(*args, **kwargs)
 
@@ -11,9 +14,18 @@ class Transformer(ABC):
         pass
 
 
-class Feature(Transformer):
+class Preprocessor(Transformer):
+    """ A Preprocessor class is a Transformer class whose transform
+    method takes a dictionary of data and returns that same
+    dictionary after some modification. The Preprocessor overwrites.
+    """
     pass
 
 
-class Preprocessor(Transformer):
+class Feature(Transformer):
+    """ A Feature class is a Transformer class whose transform
+    method takes a dictionary of data and returns a new
+    data object. The Feature creates something new respecting
+    the existing data.
+    """
     pass
