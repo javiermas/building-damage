5875998545d712e658d65f8999af561a62c1b20d
javiermas <javier.mas@barcelonagse.eu>
2019-05-14 19:05:30 +0200

    :sparkles: :wrench: Add new method to merge feature data and change pipeline to take tuples of (f_name, features)

--- damage/features/pipeline.py
+++ damage/features/pipeline.py
@@ -1,3 +1,6 @@
+from functools import reduce
+import pandas as pd
+
 from damage.features.base import Transformer
 
 
@@ -5,13 +8,19 @@ class Pipeline(Transformer):
 
     def __init__(self, features, preprocessors):
         self.features = features
+        self.feature_names = [feature_name for feature_name, _ in self.features]
         self.preprocessors = preprocessors
 
     def transform(self, data):
-        for preprocessor in self.preprocessors:
-            data = preprocessor.transform(data)
+        for preprocessor_name, preprocessor in self.preprocessors:
+            data = preprocessor(data)
 
-        for feature in self.features:
-            data[feature.__class__.__name__] = feature.transform(data)
+        for feature_name, feature in self.features:
+            data[feature_name] = feature(data)
 
-        return {feature.__class__.__name__: data[feature.__class__.__name__] for feature in self.features}
+        feature_data = [data[name] for name in self.feature_names if name in data.keys()]
+        feature_data = self._merge_feature_data(feature_data)
+        return feature_data
+
+    def _merge_feature_data(self, feature_data):
+        return reduce(lambda l, r: pd.merge(l, r, left_index=True, right_index=True, how='outer'), feature_data)
