ea279a12a6fbbb4867ffdbc7b95efbb6e353e349
javiermas <javier.mas@barcelonagse.eu>
2019-05-14 19:04:57 +0200

    :bulb: Set up logging for transformers

--- damage/features/base.py
+++ damage/features/base.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+import logging
+from uuid import uuid4
 
 
 class Transformer(ABC):
@@ -6,13 +8,25 @@ class Transformer(ABC):
     that takes a dictionary of data and returns some transformation
     of that data.
     """
+    def __init__(self):
+        self._set_up_logging()
+
     def __call__(self, *args, **kwargs):
+        self.log.info('Applying {}'.format(self.__class__.__name__))
         return self.transform(*args, **kwargs)
 
     @abstractmethod
     def transform(self, data):
         pass
 
+    def _set_up_logging(self):
+        class_name = self.__class__.__name__
+        self.log = logging.getLogger(class_name + str(uuid4()))
+        self.log.setLevel('INFO')
+        #handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(levelname)s:::{}:::%(asctime)s:::%(message)s'.format(class_name))
+        #handler.setFormatter(formatter)
+        #self.log.addHandler(handler)
 
 class Preprocessor(Transformer):
     """ A Preprocessor class is a Transformer class whose transform
