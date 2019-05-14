0fedc9028083032d5e783e066f3499038229ab55
javiermas <javier.mas@barcelonagse.eu>
2019-05-14 19:06:06 +0200

    :wrench: Change date to raster_date when doing operations on RasterPairMaker

--- damage/features/raster_pair_maker.py
+++ damage/features/raster_pair_maker.py
@@ -1,20 +1,22 @@
 import pandas as pd
-from numpy import concatenate
+import numpy as np
 
 from damage.features.base import Feature
 
 
 class RasterPairMaker(Feature):
 
-    """ This class makes pairs of rasters in different points in time
-    by concatenating the first raster to every posterior one in each city
+    """ This class makes pairs of raster images in different points in time
+    by concatenating the first image to every posterior one in each city
     (going from 3 to 6 channels).
     """
 
     def transform(self, data):
-        raster_pairs = self._make_raster_pairs_all_cities(data['RasterSplitter'])
+        raster_pairs = self._make_raster_pairs_all_cities(data['RasterSplitter'].reset_index())
         data.pop('RasterSplitter') # Hack to avoid memory problems
-        return raster_pairs
+        # We drop nans on date because those are the images that come before
+        # any annotation, and cannot be used for training
+        return raster_pairs.dropna(subset=['date']).set_index(['city', 'patch_id', 'date'])
 
     def _make_raster_pairs_all_cities(self, raster_data):
         raster_pairs_all_cities = []
@@ -23,22 +25,22 @@ class RasterPairMaker(Feature):
             raster_pairs_single_city = self._make_raster_pairs_single_city(raster_data_single_city)
             raster_pairs_all_cities.append(raster_pairs_single_city)
 
-        return pd.concat(raster_pairs_all_cities)
+        return pd.concat(raster_pairs_all_cities).reset_index()
 
     def _make_raster_pairs_single_city(self, raster_data):
         """ This method creates pairs of images
         (image_0, image_1), (image_0, image_2) by concatenating
         on the third axis (3 to 6 channel images).
         """
-        dates = raster_data['date'].unique()
-        first_date = raster_data['date'].min()
-        first_raster = raster_data.loc[raster_data['date'] == first_date]
+        dates = raster_data['raster_date'].unique()
+        first_date = raster_data['raster_date'].min()
+        first_raster = raster_data.loc[raster_data['raster_date'] == first_date]
         combined_rasters = []
         for date in dates:
             if date == first_date:
                 continue
 
-            single_raster = raster_data.loc[raster_data['date'] == date]
+            single_raster = raster_data.loc[raster_data['raster_date'] == date]
             combined_raster = self._make_single_raster_pair(first_raster, single_raster)
             combined_rasters.append(combined_raster)
 
@@ -52,14 +54,10 @@ class RasterPairMaker(Feature):
         concatenates the images of the common_raster to the ones
         in the variable_raster.
         """
-        combined_raster = pd.merge(variable_raster,
-                                   common_raster[['location_index', 'image']],
-                                   on='location_index')
+        combined_raster = pd.merge(variable_raster.reset_index(),
+                                   common_raster.reset_index()[['patch_id', 'image']],
+                                   on='patch_id')
         combined_raster['image'] = combined_raster.apply(
-            lambda x: concatenate([x['image_x'], x['image_y']], axis=2), axis=1)
+            lambda x: np.concatenate([x['image_x'], x['image_y']], axis=2), axis=1)
         combined_raster = combined_raster.drop(['image_x', 'image_y'], axis=1)
         return combined_raster
-
-
-
-
