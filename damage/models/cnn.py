117b8344a2d23c5472717e110e1ac8694413870c
javiermas <javier.mas@barcelonagse.eu>
2019-05-15 13:18:56 +0200

    :sparkles: Allow for weights to be passed to CNN model

--- damage/models/cnn.py
+++ damage/models/cnn.py
@@ -9,21 +9,22 @@ from damage.models.base import Model
 class CNN(Model):
 
     def __init__(self, convolutional_layers, dense_units=64, learning_rate=0.1,
-                 num_classes=2, weight_positives=1, **kwargs):
+                 num_classes=2, loss_weights=None, **kwargs):
         self.convolutional_layers = convolutional_layers
         self.dense_units = dense_units
         self.learning_rate = learning_rate
         self.num_classes = num_classes
+        self.loss_weights = loss_weights
         self.model = self._create_model()
 
     def fit_generator(self, generator, epochs, steps_per_epoch, **kwargs):
         self.model.fit_generator(generator, epochs=epochs, steps_per_epoch=steps_per_epoch)
 
     def validate_generator(self, train_generator, test_generator, validation_steps,
-                           epochs, steps_per_epoch, **kwargs):
+                           epochs, steps_per_epoch, class_weight=None, **kwargs):
         model_fit = self.model.fit_generator(train_generator, validation_data=test_generator,
                                              epochs=epochs, validation_steps=validation_steps,
-                                             steps_per_epoch=steps_per_epoch)
+                                             steps_per_epoch=steps_per_epoch, class_weight=class_weight)
         return model_fit.history
 
     def _create_model(self):
@@ -40,7 +41,8 @@ class CNN(Model):
         model = Sequential(layers)
         model.compile(optimizer='adam', loss='binary_crossentropy', learning_rate=self.learning_rate,
                       metrics=['accuracy', precision, recall, true_positives, true_negatives,
-                               false_negatives, false_positives, positives, negatives])
+                               false_negatives, false_positives, positives, negatives],
+                      loss_weights=self.loss_weights)
         return model
 
     @staticmethod
