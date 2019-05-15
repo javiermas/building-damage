079aed4cae6da31fd68229ec8aab580780b3a1d8
javiermas <javier.mas@barcelonagse.eu>
2019-05-15 13:18:38 +0200

    :sparkles: Create new script predict_cnn and remove comments from validate_cnn

--- scripts/predict_cnn.py
+++ scripts/predict_cnn.py
@@ -48,24 +48,25 @@ def read_no_analysis_areas(file_names):
 RASTERS_PATH = 'data/city_rasters'
 ANNOTATIONS_PATH = 'data/annotations'
 POLYGONS_PATH = 'data/polygons'
-RESULTS_PATH = 'logs/experiments'
+RESULTS_PATH = 'logs'
 
-city = 'Daraa'
+city = 'Aleppo'
 annotation_files = [
-    '4_Damage_Sites_{}_CDA.shp'.format(city),
-    #'6_Damage_Sites_{}_SDA.shp'.format(city),
+    #'4_Damage_Sites_{}_CDA.shp'.format(city),
+    '6_Damage_Sites_{}_SDA.shp'.format(city),
 ]
 annotation_data = read_annotations(file_names=['{}/{}'.format(ANNOTATIONS_PATH, f) for f in annotation_files])
 
 raster_files = [
-    'daraa_2011_10_17_zoom_19.tif',
-    'daraa_2017_02_07_zoom_19.tif',
+    #'daraa_2011_10_17_zoom_19.tif',
+    #'daraa_2017_02_07_zoom_19.tif',
     #'homs_2011_05_21_zoom_19.tif',
     #'homs_2016_05_30_zoom_19.tif',
     #'raqqa_2013_01_17_zoom_19.tif',
     #'raqqa_2016_07_01_zoom_19.tif',
-    #'aleppo_2011_06_26_zoom_19.tif',
-    #'aleppo_2016_10_19_zoom_19.tif',
+    'aleppo_2011_06_26_zoom_19.tif',
+    'aleppo_2016_09_18_zoom_19.tif',
+    'aleppo_2016_10_19_zoom_19.tif',
 ]
 raster_data = read_rasters(file_names=['{}/{}'.format(RASTERS_PATH, f) for f in raster_files])
 
@@ -95,26 +96,42 @@ pipeline = features.Pipeline(
 features = pipeline.transform(data)
 
 #### Modelling
-random_search = RandomSearch()
+import ipdb; ipdb.set_trace()
+experiment_files = os.listdir('{}/experiments'.format(RESULTS_PATH))
+experiment_results = []
+for file_name in experiment_files:
+    with open('{}/experiments/{}'.format(RESULTS_PATH, file_name), 'r') as f:
+        try:
+            result = eval(json.load(f))
+            result['id'] = int(file_name.split('_')[1].split('.')[0])
+            experiment_results.append(result)
+        except:
+            continue
+
+import ipdb; ipdb.set_trace()
+experiment_results = pd.DataFrame(experiment_results)
+space = experiment_results.loc[experiment_results['id'].idxmax(), 'space']
 os.environ['CUDA_VISIBLE_DEVICES'] = '5'
 Model = CNN
-spaces = random_search.sample_cnn(10)
-for space in spaces:
-    data_stream = DataStream(batch_size=space['batch_size'], test_proportion=0.8)
-    num_batches = ceil(len(features) / space['batch_size'])
-    train_generator, test_generator = data_stream.split(np.stack(features['image'].values),
-                                                        features['destroyed'].values)
-    space['class_weight'] = {
-        0: features['destroyed'].mean(),
-        1: 1 - features['destroyed'].mean(),
-    }
-    cnn = Model(**space)
-    losses = cnn.validate_generator(train_generator, test_generator,
-                                    steps_per_epoch=num_batches,
-                                    validation_steps=1,
-                                    **space)
-    losses['model'] = Model.__class__.__name__
-    losses['space'] = space
-    losses['patch_size'] = patch_size
-    with open('{}/experiment_{}.json'.format(RESULTS_PATH, round(time())), 'w') as f:
-        json.dump(str(losses), f)
+data_stream = DataStream(batch_size=space['batch_size'], test_proportion=0.8)
+num_batches = ceil(len(features) / space['batch_size'])
+last_date = features.reset_index()['date'].max()
+features_train = features.loc[features.index.get_level_values('date') < last_date]
+features_predict = features.xs(last_date, level='date')
+train_generator = data_stream.get_single_generator(np.stack(features_train['image'].values),
+                                                   features_train['destroyed'].values)
+space['class_weight'] = {
+    0: features['destroyed'].mean(),
+    1: 1 - features['destroyed'].mean(),
+}
+cnn = Model(**space)
+losses = cnn.fit_generator(train_generator,
+                           steps_per_epoch=num_batches,
+                           validation_steps=1,
+                           **space)
+
+predictions = cnn.predict(features_predict['image'])
+predictions = pd.DataFrame({
+    'prediction': predictions,
+}, features_predict.index)
+predictions.to_pickle('{}/predictions/prediction_{}.p'.format(RESULTS_PATH, time()))
