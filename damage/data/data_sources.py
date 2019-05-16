f329306b9b324fe6ea8a1314a92194a536cc435c
javiermas <javier.mas@barcelonagse.eu>
2019-05-16 10:39:27 +0200

    :sparkles: Create file with DATA_SOURCES

---
+++ damage/data/data_sources.py
@@ -0,0 +1,29 @@
+DATA_SOURCES = {
+    'aleppo': {
+        'annotations': [
+            '6_Damage_Sites_Aleppo_SDA.shp',
+        ],
+        'rasters': [
+            'aleppo_2011_06_26_zoom_19.tif',
+            'aleppo_2016_09_18_zoom_19.tif',
+            'aleppo_2016_10_19_zoom_19.tif',
+        ],
+        'no_analysis': [
+            '5_No_Analysis_Areas_Aleppo.shp',
+        ],
+    },
+    'daraa': {
+        'annotations': [
+            '4_Damage_Sites_Daraa_CDA.shp',
+        ],
+        'rasters': [
+            'daraa_2011_10_17_zoom_19.tif',
+            'daraa_2017_02_07_zoom_19.tif',
+        ],
+        'no_analysis': [
+            '5_No_Analysis_Areas_Daraa.shp',
+        ],
+    },
+}
+
+
