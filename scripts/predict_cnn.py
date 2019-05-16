380773bb1957cb647f6eeb16f7c440276670b8da
javiermas <javier.mas@barcelonagse.eu>
2019-05-16 10:40:28 +0200

    :wrench: Refactor predict_cnn

--- scripts/predict_cnn.py
+++ scripts/predict_cnn.py
@@ -1,47 +1,14 @@
 import os
 import json
-import geopandas as gpd
-import numpy as np
-import pandas as pd
 from math import ceil
 from time import time
+import numpy as np
+import pandas as pd
 
-from damage.data import DataStream, Raster
+from damage.data import read_annotations, read_populated_areas, read_rasters, read_no_analysis_areas
+from damage.data.data_sources import DATA_SOURCES
 from damage.models import CNN, RandomSearch
 from damage import features
-
-
-def read_annotations(file_names):
-    data = {}
-    for file_name in file_names:
-        data['annotation_'+file_name.split('/')[-1]] = gpd.read_file(file_name)
-
-    return data
-
-
-def read_rasters(file_names):
-    data = {}
-    for file_name in file_names:
-        data['raster_'+file_name.split('/')[-1]] = Raster(path=file_name)
-
-    return data
-
-
-def read_populated_areas(file_names):
-    data = {}
-    for file_name in file_names:
-        populated_area = gpd.read_file(file_name)
-        populated_area['NAME_EN'] = populated_area['NAME_EN'].str.lower()
-        data['populated_areas_'+file_name.split('/')[-1]] = populated_area
-
-    return data
-
-def read_no_analysis_areas(file_names):
-    data = {}
-    for file_name in file_names:
-        data['no_analysis_areas_'+file_name.split('/')[-1]] = gpd.read_file(file_name)
-
-    return data
 
 
 ## Reading
@@ -50,37 +17,34 @@ ANNOTATIONS_PATH = 'data/annotations'
 POLYGONS_PATH = 'data/polygons'
 RESULTS_PATH = 'logs'
 
-city = 'Aleppo'
-annotation_files = [
-    #'4_Damage_Sites_{}_CDA.shp'.format(city),
-    '6_Damage_Sites_{}_SDA.shp'.format(city),
-]
-annotation_data = read_annotations(file_names=['{}/{}'.format(ANNOTATIONS_PATH, f) for f in annotation_files])
-
-raster_files = [
-    #'daraa_2011_10_17_zoom_19.tif',
-    #'daraa_2017_02_07_zoom_19.tif',
-    #'homs_2011_05_21_zoom_19.tif',
-    #'homs_2016_05_30_zoom_19.tif',
-    #'raqqa_2013_01_17_zoom_19.tif',
-    #'raqqa_2016_07_01_zoom_19.tif',
-    'aleppo_2011_06_26_zoom_19.tif',
-    'aleppo_2016_09_18_zoom_19.tif',
-    'aleppo_2016_10_19_zoom_19.tif',
-]
-raster_data = read_rasters(file_names=['{}/{}'.format(RASTERS_PATH, f) for f in raster_files])
+cities = ['aleppo']
+data = {}
+for city in cities:
+    import ipdb; ipdb.set_trace()
+    annotation_files = ['{}/{}'.format(ANNOTATIONS_PATH, f) for f in DATA_SOURCES['aleppo']['annotations']]
+    annotation_data = read_annotations(file_names=annotation_files)
+    raster_files = ['{}/{}'.format(RASTERS_PATH, f) for f in DATA_SOURCES['aleppo']['rasters']]
+    raster_data = read_rasters(file_names=raster_files)
+    no_analysis_files = ['{}/{}'.format(POLYGONS_PATH, f) for f in DATA_SOURCES['aleppo']['no_analysis']]
+    no_analysis_area = read_no_analysis_areas(file_names=no_analysis_files)
+    data = {**data, **annotation_data, **raster_data, **no_analysis_area}
 
 populated_areas = read_populated_areas(file_names=[
     '{}/populated_areas.shp'.format(POLYGONS_PATH),
 ])
-no_analysis_area = read_no_analysis_areas(file_names=[
-    '{}/5_No_Analysis_Areas_{}.shp'.format(POLYGONS_PATH, city),
-])
 
-data = {**annotation_data, **raster_data, **populated_areas, **no_analysis_area}
+data = {**populated_areas, **data}
+
+raster_files = [
+    #'homs_2011_05_21_zoom_19.tif',
+    #'homs_2016_05_30_zoom_19.tif',
+    #'raqqa_2013_01_17_zoom_19.tif',
+    #'raqqa_2016_07_01_zoom_19.tif',
+]
+
 ### Processing
 grid_size = 0.035
-patch_size = 64*5
+patch_size = 64*10
 stride = patch_size#16
 pipeline = features.Pipeline(
     preprocessors=[
@@ -116,6 +80,7 @@ Model = CNN
 data_stream = DataStream(batch_size=space['batch_size'], test_proportion=0.8)
 num_batches = ceil(len(features) / space['batch_size'])
 last_date = features.reset_index()['date'].max()
+import ipdb; ipdb.set_trace()
 features_train = features.loc[features.index.get_level_values('date') < last_date]
 features_predict = features.xs(last_date, level='date')
 train_generator = data_stream.get_single_generator(np.stack(features_train['image'].values),
