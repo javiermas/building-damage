e5d9fe0e6ad0927bb142b852f3a82e82200e6517
javiermas <javier.mas@barcelonagse.eu>
2019-05-19 13:43:53 +0200

    :sparkles: Create method split by patch id

--- damage/data/data_stream.py
+++ damage/data/data_stream.py
@@ -4,26 +4,31 @@ import numpy as np
 
 
 class DataStream:
-    def __init__(self, batch_size, test_proportion):
+    def __init__(self, batch_size, train_proportion):
         self.batch_size = batch_size
-        self.test_proportion = test_proportion
+        self.train_proportion = train_proportion
         self.current_batch_num = 0
 
-    def split(self, x, y):
-        train_index = random.sample(range(len(y)), round(len(y)*0.8))
-        train_generator = self.get_single_generator(x[train_index], y[train_index], self.batch_size)
-        test_generator = self.get_single_generator(np.delete(x, train_index, axis=0),
-                                                   np.delete(y, train_index, axis=0),
-                                                   len(y) - len(train_index))
-        return train_generator, test_generator
+    def split_by_patch_id(self, x, y):
+        unique_patches = x.index.get_level_values('patch_id').unique().tolist()
+        train_patches = random.sample(unique_patches, round(len(unique_patches)*self.train_proportion))
+        x_train = np.stack(x.loc[x.index.get_level_values('patch_id').isin(train_patches)].values)
+        y_train = y.loc[y.index.get_level_values('patch_id').isin(train_patches)].values
+        train_generator = self.get_single_generator(x_train, y_train, self.batch_size)
+
+        test_patches = [patch for patch in unique_patches if patch not in train_patches]
+        x_test = np.stack(x.loc[x.index.get_level_values('patch_id').isin(test_patches)].values)
+        y_test = y.loc[y.index.get_level_values('patch_id').isin(test_patches)].values
+        test_generator = self.get_single_generator(x_test, y_test, self.batch_size)
+        return train_generator, test_generator, test_patches
 
     def get_single_generator(self, x, y, batch_size):
         num_batches = ceil(len(y) / batch_size)
         while True:
             for i in range(num_batches):
                 batch = (
-                    x[(i*batch_size): ((i+1)*self.batch_size)],
-                    y[(i*batch_size): ((i+1)*self.batch_size)]
+                    x[(i*batch_size): ((i+1)*self.batch_size)].astype(float),
+                    y[(i*batch_size): ((i+1)*self.batch_size)].astype(float)
                 )
                 yield batch
 
