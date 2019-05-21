023da98153d53c59668d3958d920ad15819e81c0
javiermas <javier.mas@barcelonagse.eu>
2019-05-21 21:07:47 +0200

    :wrench: Adapt scripts to new compute features script

--- scripts/predict_cnn.py
+++ scripts/predict_cnn.py
@@ -1,36 +1,26 @@
 import os
 from math import ceil
 from time import time
+import argparse
 import pandas as pd
 
 from damage.models import CNN
-from damage.data import DataStream, load_data_multiple_cities, load_experiment_results
+from damage.data import DataStream, load_experiment_results
 from damage import features
 
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '5'
+parser = argparse.ArgumentParser()
+parser.add_argument('features')
+parser.add_argument('--gpu')
+args = vars(parser.parse_args())
+
+os.environ['CUDA_VISIBLE_DEVICES'] = args.get('gpu', None) or '5'
 RESULTS_PATH = 'logs/predictions'
+FEATURES_PATH = 'logs/features'
+features_file_name = args.get('features')
 
-## Reading
-cities = ['daraa']
-data = load_data_multiple_cities(cities)
-
-### Processing
-grid_size = 0.035
-patch_size = 64*10
-stride = patch_size#16
-pipeline = features.Pipeline(
-    preprocessors=[
-        ('AnnotationPreprocessor', features.AnnotationPreprocessor(grid_size=grid_size)),
-    ],
-    features=[
-        ('RasterSplitter', features.RasterSplitter(patch_size=patch_size, stride=stride, grid_size=grid_size)),
-        ('AnnotationMaker', features.AnnotationMaker()),
-        ('RasterPairMaker', features.RasterPairMaker()),
-    ],
-
-)
-features = pipeline.transform(data)
+# Reading
+features = pd.read_pickle('{}/{}'.format(FEATURES_PATH, features_file_name))
 
 #### Modelling
 Model = CNN
@@ -45,10 +35,10 @@ space['class_weight'] = {
 
 data_stream = DataStream(batch_size=space['batch_size'], train_proportion=0.6)
 train_index_generator, test_index_generator = data_stream.split_by_patch_id(features['image'])
-train_generator = data_stream.get_train_data_generator_from_index(features['image'], features['destroyed'],
-                                                                  train_index_generator)
+train_generator = data_stream.get_data_generator_from_index([features['image'], features['destroyed']],
+                                                            train_index_generator)
 test_indices = list(test_index_generator)
-test_generator = data_stream.get_test_data_generator_from_index(features['image'], test_indices)
+test_generator = data_stream.get_data_generator_from_index([features['image']], test_indices)
 
 num_batches = ceil(len(features) / space['batch_size'])
 model = Model(**space)
--- scripts/validate_cnn.py
+++ scripts/validate_cnn.py
@@ -3,65 +3,50 @@ import argparse
 import json
 from math import ceil
 from time import time
-import numpy as np
+import pandas as pd
 
-from damage.data import DataStream, load_data_multiple_cities
+from damage.data import DataStream
 from damage.models import CNN, RandomSearch
-from damage import features
 
 parser = argparse.ArgumentParser()
+parser.add_argument('features')
 parser.add_argument('--gpu')
 args = vars(parser.parse_args())
 
 
 os.environ['CUDA_VISIBLE_DEVICES'] = args.get('gpu', None) or '5'
 RESULTS_PATH = 'logs/experiments'
+FEATURES_PATH = 'logs/features'
+features_file_name = args.get('features')
 
-## Reading
-cities = ['daraa']
-data = load_data_multiple_cities(cities)
-
-### Processing
-grid_size = 0.035
-patch_size = 64*10
-stride = patch_size
-pipeline = features.Pipeline(
-    preprocessors=[
-        ('AnnotationPreprocessor', features.AnnotationPreprocessor(grid_size=grid_size)),
-    ],
-    features=[
-        ('RasterSplitter', features.RasterSplitter(patch_size=patch_size, stride=stride, grid_size=grid_size)),
-        ('AnnotationMaker', features.AnnotationMaker()),
-        ('RasterPairMaker', features.RasterPairMaker()),
-    ],
-
-)
-features = pipeline.transform(data)
+# Reading
+features = pd.read_pickle('{}/{}'.format(FEATURES_PATH, features_file_name))
 
 #### Modelling
 sampler = RandomSearch()
 Model = CNN
 spaces = sampler.sample_cnn(10)
 for space in spaces:
+    space['epochs'] = 1
     data_stream = DataStream(batch_size=space['batch_size'], train_proportion=0.8)
     num_batches = ceil(len(features) / space['batch_size'])
     train_index_generator, test_index_generator = data_stream.split_by_patch_id(features['image'])
-    train_generator = data_stream.get_train_data_generator_from_index(features['image'], features['destroyed'],
-                                                                      train_index_generator)
+    train_generator = data_stream.get_data_generator_from_index(
+        [features['image'], features['destroyed']], train_index_generator)
     test_indices = list(test_index_generator)
-    test_generator = data_stream.get_train_data_generator_from_index(features['image'], features['destroyed'],
-                                                                     test_indices)
+    test_generator = data_stream.get_data_generator_from_index(
+        [features['image'], features['destroyed']], test_indices)
     space['class_weight'] = {
         0: features['destroyed'].mean(),
         1: 1 - features['destroyed'].mean(),
     }
     model = Model(**space)
     losses = model.validate_generator(train_generator, test_generator,
-                                     steps_per_epoch=num_batches,
-                                     validation_steps=1,
-                                     **space)
+                                      steps_per_epoch=num_batches,
+                                      validation_steps=1,
+                                      **space)
     losses['model'] = str(Model)
     losses['space'] = space
-    losses['patch_size'] = patch_size
-    with open('{}/experiments/experiment_{}.json'.format(RESULTS_PATH, round(time())), 'w') as f:
+    losses['features'] = features_file_name
+    with open('{}/experiment_{}.json'.format(RESULTS_PATH, round(time())), 'w') as f:
         json.dump(str(losses), f)
