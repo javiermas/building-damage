14f3ad6954468ec702c53b4c2533ba90e2da9de8
javiermas <javier.mas@barcelonagse.eu>
2019-05-25 11:21:25 +0200

    :bulb: Add docstrings to Pipeline

--- damage/features/pipeline.py
+++ damage/features/pipeline.py
@@ -5,13 +5,39 @@ from damage.features.base import Transformer
 
 
 class Pipeline(Transformer):
+    """ The Pipeline class takes a list of tuples (name, function)
+    for preprocessors and features and applies them with
+    the transform method. The feature functions need to return
+    a dataframe with an identically structured index so the
+    merge can be performed (e.g. city, patch_id)
+    """
 
-    def __init__(self, features, preprocessors):
+    def __init__(self, preprocessors, features):
+        self.preprocessors = preprocessors
         self.features = features
         self.feature_names = [feature_name for feature_name, _ in self.features]
-        self.preprocessors = preprocessors
 
     def transform(self, data):
+        """ The transform method takes a dictionary of data where
+        each key represents a different data source (e.g. annotations)
+        and each value a data object (e.g. pandas dataframe). The transform
+        method iterates first over the preprocessor functions, overwriting
+        the data object. Then, it iterates over the feature functions
+        creating new keys in the data dictionary with the passed name.
+        That way, features can use data generates by previously computed
+        features.
+
+        input
+        -------
+        data (dict): dictionary of data with data sources as keys
+        and data objects as values.
+
+        output
+        -------
+        feature_data (pandas.DataFrame): dataframe contained
+        the merged data outputed by the feature functions.
+        """
+
         for preprocessor_name, preprocessor in self.preprocessors:
             data = preprocessor(data)
 
