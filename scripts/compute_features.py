f5cabb02ec8bf6fc86710e70f73311cc6a0cb179
javiermas <javier.mas@barcelonagse.eu>
2019-05-27 13:29:30 +0200

    :sparkles: Adapt scripts to storing and loading of patches

--- scripts/compute_features.py
+++ scripts/compute_features.py
@@ -1,23 +1,19 @@
 import argparse
+import os
 from time import time
 
-from damage.data import load_data_multiple_cities
+from damage.data import load_data_multiple_cities, DataStream
 from damage import features
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--filename')
-args = vars(parser.parse_args())
-
 STORING_PATH = 'logs/features'
-file_name = args.get('filename', None) or '{}.p'.format(str(round(time())))
 ## Reading
 cities = ['daraa']
 data = load_data_multiple_cities(cities)
 
 ### Processing
 grid_size = 0.035
-patch_size = 64*10
+patch_size = 64
 stride = patch_size
 pipeline = features.Pipeline(
     preprocessors=[
@@ -30,5 +26,34 @@ pipeline = features.Pipeline(
     ],
 
 )
+
 features = pipeline.transform(data)
-features.to_pickle('{}/{}'.format(STORING_PATH, file_name))
+import ipdb; ipdb.set_trace()
+# Store batches
+BATCH_SIZE = 50
+TRAIN_PROPORTION = 0.7
+CLASS_PROPORTION = {
+    1: 0.4,
+}
+FOLDER_NAME = '_'.join(c for c in cities)
+
+data_stream = DataStream(BATCH_SIZE, TRAIN_PROPORTION, CLASS_PROPORTION)
+train_index_generator, test_index_generator = data_stream.split_by_patch_id(features['image'], features['destroyed'])
+
+
+if not os.path.exists('{}/{}'.format(STORING_PATH, FOLDER_NAME)):
+    os.mkdir('{}/{}'.format(STORING_PATH, FOLDER_NAME))
+    os.mkdir('{}/{}/train'.format(STORING_PATH, FOLDER_NAME))
+    os.mkdir('{}/{}/test'.format(STORING_PATH, FOLDER_NAME))
+
+data_stream.store_data_batches_from_index(
+    path='{}/{}/train'.format(STORING_PATH, FOLDER_NAME),
+    data=[features['image'], features['destroyed']],
+    index=train_index_generator
+)
+
+data_stream.store_data_batches_from_index(
+    path='{}/{}/test'.format(STORING_PATH, FOLDER_NAME),
+    data=[features['image'], features['destroyed']],
+    index=test_index_generator
+)
