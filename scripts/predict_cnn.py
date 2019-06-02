d0ee6900255cf2017974dc6b5173a2d6409afb43
Javier Mas <javier.mas@barcelonagse.eu>
2019-06-02 18:40:58 +0200

    :sparkles: Develop scripts exec

--- scripts/predict_cnn.py
+++ scripts/predict_cnn.py
@@ -6,7 +6,7 @@ import logging
 from functools import reduce
 import pandas as pd
 
-from damage.models import CNN
+from damage.models import CNN, RandomSearch
 from damage.data import DataStream, load_experiment_results
 from damage import features
 
@@ -23,30 +23,36 @@ features_file_name = args.get('features')
 
 # Reading
 features = pd.read_pickle('{}/{}'.format(FEATURES_PATH, features_file_name))
+features_destroyed = features.loc[features['destroyed'] == 1].sample(1000)
+features_non_destroyed = features.loc[features['destroyed'] == 0].sample(1000)
+features = pd.concat([features_destroyed, features_non_destroyed])
 
 #### Modelling
 Model = CNN
 
 # Choose space
 experiment_results = load_experiment_results()
-experiment_results_single_model = experiment_results.loc[experiment_results['model'] == str(Model)]
-space = experiment_results_single_model.loc[experiment_results_single_model['id'].idxmax(), 'space']
+if not experiment_results.empty:
+    experiment_results_single_model = experiment_results.loc[experiment_results['model'] == str(Model)]
+    space = experiment_results_single_model.loc[experiment_results_single_model['id'].idxmax(), 'space']
+else:
+    space = RandomSearch._sample_single_cnn_space()
+
 class_proportion = {
-    1: 0.1,
+    1: 0.3,
 }
 space['class_weight'] = {
-    0: class_proportion[1],
-    1: 1 - class_proportion[1],
+    0: (class_proportion[1] -0.1),
+    1: 1 - (class_proportion[1] -0.1),
 }
-space['epochs'] = 10
 # Get data generators
-data_stream = DataStream(batch_size=space['batch_size'], train_proportion=0.6,
+data_stream = DataStream(batch_size=space['batch_size'], train_proportion=0.7,
                          class_proportion=class_proportion)
 train_index_generator, test_index_generator = data_stream.split_by_patch_id(features['image'], features['destroyed'])
-train_generator = data_stream.get_data_generator_from_index([features['image'], features['destroyed']],
+train_generator = data_stream.get_train_data_generator_from_index([features['image'], features['destroyed']],
                                                             train_index_generator)
 test_indices = list(test_index_generator)
-test_generator = data_stream.get_data_generator_from_index([features['image']], test_indices)
+test_generator = data_stream.get_test_data_generator_from_index(features['image'], test_indices)
 
 num_batches = ceil(len(features) / space['batch_size'])
 # Fit model and predict
@@ -56,10 +62,16 @@ model.fit_generator(train_generator,
                     validation_steps=1,
                     **space)
 
-predictions = model.predict_generator(test_generator, steps=len(test_indices))
+import ipdb; ipdb.set_trace()
+predictions = model.predict_generator(test_generator)#, steps)#€=len(test_indices))
+
+test_indices_flattened = test_indices[0]
+for index in test_indices[1:]:
+    test_indices_flattened = test_indices_flattened.append(index)
+
 predictions = pd.DataFrame({
-    'prediction': predictions[:, 1],
-}, index=reduce(lambda l, r: l.union(r), test_indices))
+    'prediction': predictions.reshape(-1),
+}, index=test_indices_flattened)
 file_name = '{}/prediction_{}.p'.format(RESULTS_PATH, round(time()))
 predictions.to_pickle(file_name)
 print('Store predictions on file: {}'.format(file_name))
