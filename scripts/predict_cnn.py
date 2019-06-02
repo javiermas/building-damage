6eb7fb4fa16ec2ce4528bae6da6b2cdf045c6e3f
javiermas <javier.mas@barcelonagse.eu>
2019-06-02 16:43:38 +0200

    :bug: Fix bug storing predictions

--- scripts/predict_cnn.py
+++ scripts/predict_cnn.py
@@ -2,7 +2,6 @@ import os
 from math import ceil
 from time import time
 import argparse
-import logging
 from functools import reduce
 import pandas as pd
 
@@ -10,7 +9,7 @@ from damage.models import CNN
 from damage.data import DataStream, load_experiment_results
 from damage import features
 
-   
+
 parser = argparse.ArgumentParser()
 parser.add_argument('features')
 parser.add_argument('--gpu')
@@ -32,13 +31,13 @@ experiment_results = load_experiment_results()
 experiment_results_single_model = experiment_results.loc[experiment_results['model'] == str(Model)]
 space = experiment_results_single_model.loc[experiment_results_single_model['id'].idxmax(), 'space']
 class_proportion = {
-    1: 0.1,
+    1: 0.2,
 }
 space['class_weight'] = {
     0: class_proportion[1],
     1: 1 - class_proportion[1],
 }
-space['epochs'] = 10
+space['epochs'] = 3
 # Get data generators
 data_stream = DataStream(batch_size=space['batch_size'], train_proportion=0.6,
                          class_proportion=class_proportion)
@@ -58,7 +57,7 @@ model.fit_generator(train_generator,
 
 predictions = model.predict_generator(test_generator, steps=len(test_indices))
 predictions = pd.DataFrame({
-    'prediction': predictions[:, 1],
+    'prediction': predictions.reshape(-1),
 }, index=reduce(lambda l, r: l.union(r), test_indices))
 file_name = '{}/prediction_{}.p'.format(RESULTS_PATH, round(time()))
 predictions.to_pickle(file_name)
