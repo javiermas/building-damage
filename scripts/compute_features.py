012df771ed2477fc210332d2cd5211f344cbc12f
javiermas <javier.mas93@gmail.com>
2019-06-08 13:52:24 +0200

    :sparkles: Add storing method that doesnt brake with large pickles

--- scripts/compute_features.py
+++ scripts/compute_features.py
@@ -1,4 +1,6 @@
 import argparse
+import pickle
+import sys
 from time import time
 
 from damage.data import load_data_multiple_cities
@@ -31,5 +33,16 @@ pipeline = features.Pipeline(
 
 )
 features = pipeline.transform(data)
-features.to_pickle('{}/{}'.format(STORING_PATH, file_name))
+def save_as_pickled_object(obj, filepath):
+    """
+    This is a defensive way to write pickle.write, allowing for very large files on all platforms
+    """
+    max_bytes = 2**31 - 1
+    bytes_out = pickle.dumps(obj)
+    n_bytes = sys.getsizeof(bytes_out)
+    with open(filepath, 'wb') as f_out:
+        for idx in range(0, n_bytes, max_bytes):
+            f_out.write(bytes_out[idx:idx+max_bytes])
+
+save_as_pickled_object(features, '{}/{}'.format(STORING_PATH, file_name))
 print('Features stored in {}/{}'.format(STORING_PATH, file_name))
