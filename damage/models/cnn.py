51df8b63a4e329493f5caafd493c33a3d3531baa
Javier Mas <javier.mas@barcelonagse.eu>
2019-06-13 21:12:25 +0200

    :sparkles: Develop support for VGG layers in CNN class

--- damage/models/cnn.py
+++ damage/models/cnn.py
@@ -10,10 +10,16 @@ class CNN(Model):
     metrics = ['accuracy', recall, specificity, precision, negatives,
                positives, sensitivity_specificity_average]
 
-    def __init__(self, convolutional_layers, dense_units=64, learning_rate=0.1, **kwargs):
+    def __init__(self, convolutional_layers, dense_units=64, learning_rate=0.1, layer_type='cnn', **kwargs):
         self.convolutional_layers = convolutional_layers
         self.dense_units = dense_units
         self.learning_rate = learning_rate
+        self.layer_type = layer_type
+        self.layer_funcs = {
+            'cnn': self._create_convolutional_and_pooling_layer_cnn,
+            'vgg': self._create_convolutional_and_pooling_layer_vgg,
+        }
+        self._create_convolutional_and_pooling_layer = self.layer_funcs[self.layer_type]
         self.model = self._create_model()
 
     def fit_generator(self, generator, epochs, steps_per_epoch, class_weight=None, **kwargs):
@@ -47,7 +53,15 @@ class CNN(Model):
         return model
 
     @staticmethod
-    def _create_convolutional_and_pooling_layer(filters, kernel_size, pool_size, dropout=0):
+    def _create_convolutional_and_pooling_layer_vgg(filters, kernel_size, pool_size, dropout=0):
+        conv_0 = Conv2D(filters=filters, kernel_size=kernel_size, padding="same", activation='relu')
+        conv_1 = Conv2D(filters=filters, kernel_size=kernel_size, padding="same", activation='relu')
+        pool = MaxPooling2D(pool_size=pool_size, strides=1)
+        dropout = Dropout(dropout)
+        return [conv_0, conv_1, pool, dropout]
+
+    @staticmethod
+    def _create_convolutional_and_pooling_layer_cnn(filters, kernel_size, pool_size, dropout=0):
         conv = Conv2D(filters=filters, kernel_size=kernel_size, padding="same", activation='relu')
         pool = MaxPooling2D(pool_size=pool_size, strides=1)
         dropout = Dropout(dropout)
