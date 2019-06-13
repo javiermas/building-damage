621f255a4a6918ccdfffa9c54fcd175e2aaf5118
Javier Mas <javier.mas@barcelonagse.eu>
2019-06-13 20:48:14 +0200

    :sparkles: Add dropout to cnn space

--- damage/models/random_search.py
+++ damage/models/random_search.py
@@ -10,23 +10,25 @@ class RandomSearch:
 
     @staticmethod
     def _sample_single_cnn_space():
-        num_layers = random.choice(range(3, 10))
+        num_layers = random.choice(range(3, 5))
         convolutional_layers = []
-        filters = random.choice([8, 16, 32, 64])
+        filters = random.choice([8, 16, 32])
         kernel_size = random.choice([3, 5, 7, 9])
         pool_size = kernel_size-1
+        dropout = random.choice(np.linspace(0.1, 0.8, 10))
         for _ in range(num_layers):
             filters = filters*2
             layer = {
                 'kernel_size': [kernel_size, kernel_size],
                 'pool_size': [pool_size, pool_size],
                 'filters': filters,
+                'dropout': dropout,
             }
             convolutional_layers.append(layer)
 
         space = {
             'learning_rate': random.choice(np.geomspace(1e-3, 1)),
-            'batch_size': random.choice(range(100, 300)),
+            'batch_size': random.choice(range(100, 150)),
             'convolutional_layers': convolutional_layers,
             'epochs': random.choice(range(5, 25)),
         }
