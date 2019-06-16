4b931a4a6da81b52536e279e093b02d0ec7b4f15
Javier Mas <javier.mas@barcelonagse.eu>
2019-06-16 10:40:42 +0200

    :sparkles: Add test batch size to validate

--- scripts/validate.py
+++ scripts/validate.py
@@ -23,8 +23,8 @@ features_file_name = args.get('features')
 
 # Reading
 features = pd.read_pickle('{}/{}'.format(FEATURES_PATH, features_file_name)).dropna(subset=['destroyed'])
-#features_destroyed = features.loc[features['destroyed'] == 1].sample(1000)
-#features_non_destroyed = features.loc[features['destroyed'] == 0].sample(1000)
+#features_destroyed = features.loc[features['destroyed'] == 1].sample(500)
+#features_non_destroyed = features.loc[features['destroyed'] == 0].sample(5000)
 #features = pd.concat([features_destroyed, features_non_destroyed])
 
 #### Modelling
@@ -35,9 +35,12 @@ spaces = sampler.sample_cnn(50)
 class_proportion = {
     1: 0.3,
 }
-batch_size = 250
-data_stream = DataStream(batch_size=batch_size, train_proportion=0.7, class_proportion=class_proportion)
+batch_size = spaces[0]['batch_size']
+test_batch_size = 500
+data_stream = DataStream(batch_size=batch_size, train_proportion=0.7,
+                         class_proportion=class_proportion, test_batch_size=test_batch_size)
 num_batches = ceil(len(features) / batch_size)
+num_batches_test = ceil(len(test_indices)/test_batch_size)
 train_index_generator, test_index_generator = data_stream.split_by_patch_id(features[['image']],
                                                                             features[['destroyed']])
 train_generator = data_stream.get_train_data_generator_from_index(
@@ -54,13 +57,17 @@ for space in spaces:
         0: (class_proportion[1] +.1),
         1: 1 - (class_proportion[1] +.1),
     }
+    print('Now validating:\n')
+    print(space)
     model = Model(**space)
     losses = model.validate_generator(train_dataset, test_dataset,
                                       steps_per_epoch=num_batches,
-                                      validation_steps=1,
+                                      validation_steps=num_batches_test,
                                       **space)
     losses['model'] = str(Model)
     losses['space'] = space
     losses['features'] = features_file_name
+    losses['num_batches_train'] = num_batches
+    losses['num_batches_test'] = num_batches_test
     with open('{}/experiment_{}.json'.format(RESULTS_PATH, round(time())), 'w') as f:
         json.dump(str(losses), f)
