3587c1b9a6fbda675af0bc984c6eae2a586ad570
Javier Mas <javier.mas@barcelonagse.eu>
2019-06-16 10:39:49 +0200

    :sparkles: :wrench: Update random search space

--- damage/models/random_search.py
+++ damage/models/random_search.py
@@ -10,13 +10,13 @@ class RandomSearch:
 
     @staticmethod
     def _sample_single_cnn_space():
-        num_layers = random.choice(range(3, 5))
-        layer_type = random.choice(['cnn', 'vgg'])
+        num_layers = random.choice(range(2, 7))
         convolutional_layers = []
-        filters = random.choice([8, 16, 32])
+        filters = random.choice([8, 16, 32, 64])
         kernel_size = random.choice([3, 5, 7, 9])
         pool_size = kernel_size-1
         dropout = random.choice(np.linspace(0.1, 0.8, 10))
+        total_filters = filters
         for _ in range(num_layers):
             filters = filters*2
             layer = {
@@ -26,11 +26,17 @@ class RandomSearch:
                 'dropout': dropout,
             }
             convolutional_layers.append(layer)
+            total_filters += filters
 
         space = {
+            'dense_units': random.choice([16, 32, 64, 128, 256]),
             'learning_rate': random.choice(np.geomspace(1e-3, 1)),
-            'batch_size': random.choice(range(100, 150)),
+            'batch_size': random.choice(range(50, 100)),
             'convolutional_layers': convolutional_layers,
-            'epochs': random.choice(range(5, 25)),
+            'epochs': random.choice(range(5, 15)),
+            'layer_type': random.choice(['cnn', 'vgg']),
         }
+        if total_filters > 1800:
+            space = RandomSearch._sample_single_cnn_space()
+
         return space
