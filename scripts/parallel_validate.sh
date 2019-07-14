36d70c2de2bd6396fb53e5252074062acb96a54c
Javier Mas <javier.mas@barcelonagse.eu>
2019-07-14 12:08:23 +0200

    :sparkles: Add support to pass features filename in parallel_validate.sh

--- scripts/parallel_validate.sh
+++ scripts/parallel_validate.sh
@@ -1,6 +1,10 @@
+echo "Running experiments for features: ${1}"
 for var in "$@"
 do
-    nohup python scripts/validate.py aleppo.p --gpu=$var > validate_${var}.out &
+    if [ $var = $1 ]; then
+        continue
+    fi
+    nohup python scripts/validate.py --features=$1 --gpu=$var > validate_${var}.out &
     echo "Started build with gpu ${var}"
     sleep 20
 done
