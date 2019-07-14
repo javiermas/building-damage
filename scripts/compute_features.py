94bde6e1a288a1cec6ed00430dc75f87156f3b64
Javier Mas <javier.mas@barcelonagse.eu>
2019-07-14 21:08:30 +0200

    :wrench: Store method to store pickles on data folder

--- scripts/compute_features.py
+++ scripts/compute_features.py
@@ -4,19 +4,9 @@ import sys
 from time import time
 from datetime import timedelta
 
-from damage.data import load_data_multiple_cities
+from damage.data import load_data_multiple_cities, save_as_pickle_to_path
 from damage import features
 
-def save_as_pickled_object(obj, filepath):
-    """
-    This is a defensive way to write pickle.write, allowing for very large files on all platforms
-    """
-    max_bytes = 2**31 - 1
-    bytes_out = pickle.dumps(obj)
-    n_bytes = sys.getsizeof(bytes_out)
-    with open(filepath, 'wb') as f_out:
-        for idx in range(0, n_bytes, max_bytes):
-            f_out.write(bytes_out[idx:idx+max_bytes])
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--filename')
@@ -45,7 +35,7 @@ pipeline = features.Pipeline(
     ],
 )
 features = pipeline.transform(data)
-save_as_pickled_object(features, '{}/{}'.format(STORING_PATH, file_name))
-save_as_pickled_object(features[['destroyed', 'latitude', 'longitude']],
+save_as_pickle_to_path(features, '{}/{}'.format(STORING_PATH, file_name))
+save_as_pickle_to_path(features[['destroyed', 'latitude', 'longitude']],
                        '{}/target_{}'.format(STORING_PATH, file_name))
 print('Features stored in {}/{}'.format(STORING_PATH, file_name))
