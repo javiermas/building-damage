a99ab75add7060f9a13067cb94dbc3b5eed3f837
Javier Mas <javier.mas93@gmail.com>
2019-08-27 20:57:06 +0200

    :sparkles: Add iteration over class  proportions

--- scripts/validate.py
+++ scripts/validate.py
@@ -4,6 +4,7 @@ import argparse
 import json
 from time import time
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 from tensorflow.data import Dataset
 from tensorflow.errors import ResourceExhaustedError
@@ -28,10 +29,13 @@ appended_features = []
 for city_feature_filename in list_feature_filenames_by_city:
     # Reading
     features_city = pd.read_pickle('{}/{}'.format(FEATURES_PATH, city_feature_filename)).dropna(subset=['destroyed'])
+    features_destroyed = features_city.loc[features_city['destroyed'] == 1]\
+        .sample(2, replace=True)
+    features_non_destroyed = features_city.loc[features_city['destroyed'] == 0]\
+        .sample(2000, replace=True)
+    features_city = pd.concat([features_destroyed, features_non_destroyed])
     appended_features.append(features_city)
-    #features_destroyed = features.loc[features['destroyed'] == 1].sample(200)
-    #features_non_destroyed = features.loc[features['destroyed'] == 0].sample(2000)
-    #features = pd.concat([features_destroyed, features_non_destroyed])
+
 features = pd.concat(appended_features)
 
 #### Modelling
@@ -42,61 +46,76 @@ models = {
 }
 Model = random.choice([CNN])
 sample_func = models[Model]
-spaces = sample_func(500)
-# Do splits
-class_proportion = {
-    1: 0.3,
-}
-batch_size = spaces[0]['batch_size']
-test_batch_size = 500
-train_proportion = 0.7
-data_stream = DataStream(batch_size=batch_size, train_proportion=train_proportion,
-                         class_proportion=class_proportion, test_batch_size=test_batch_size)
-
-unique_patches = features.index.get_level_values('patch_id').unique().tolist()
-train_patches = random.sample(unique_patches, round(len(unique_patches)*train_proportion))
-train_data = features.loc[features.index.get_level_values('patch_id').isin(train_patches)]
-train_data_upsampled = data_stream._upsample_class_proportion(train_data, class_proportion).sample(frac=1)
-test_patches = list(set(unique_patches) - set(train_patches))
-test_data = features.loc[features.index.get_level_values('patch_id').isin(test_patches)]
-
-train_indices = data_stream._get_index_generator(train_data_upsampled, batch_size)
-test_indices = data_stream._get_index_generator(test_data, test_batch_size)
-train_generator = data_stream.get_train_data_generator_from_index(
-    [train_data_upsampled['image'], train_data_upsampled['destroyed']], train_indices)
-test_generator = data_stream.get_train_data_generator_from_index(
-    [test_data['image'], test_data['destroyed']], test_indices)
-train_dataset = Dataset.from_generator(lambda: train_generator, (tf.float32, tf.int32))
-test_dataset = Dataset.from_generator(lambda: test_generator, (tf.float32, tf.int32))
-
-num_batches = len(train_indices)
-num_batches_test = len(test_indices)
-#Validate
-for space in spaces:
-    space['batch_size'] = batch_size
-    space['class_weight'] = {
-        0: (class_proportion[1] * space['class_weight']),
-        1: 1 - (class_proportion[1] * space['class_weight']),
+class_proportions = map(float, np.linspace(0.01, 1, 5))
+print(class_proportions)
+for proportion in class_proportions:
+    class_proportion = {
+        1: proportion,
     }
-    print('Now validating:\n')
-    print(space)
-    try:
-        model = Model(**space)
-        losses = model.validate_generator(train_dataset, test_dataset,
-                                          steps_per_epoch=num_batches,
-                                          validation_steps=num_batches_test,
-                                          **space)
-    except Exception as e:
-        losses = {'log': str(e)}
+    spaces = sample_func(10)
+# Do splits
+#class_proportion = {
+#    1: 0.3,
+#}
+    batch_size = spaces[0]['batch_size']
+    test_batch_size = 500
+    train_proportion = 0.7
+    data_stream = DataStream(
+        batch_size=batch_size,
+        train_proportion=train_proportion,
+        class_proportion=class_proportion,
+        test_batch_size=test_batch_size
+    )
+    unique_patches = features.index.get_level_values('patch_id').unique().tolist()
+    train_patches = random.sample(unique_patches, round(len(unique_patches)*train_proportion))
+    train_data = features.loc[features.index.get_level_values('patch_id').isin(train_patches)]
+    train_data_upsampled = data_stream._upsample_class_proportion(train_data, class_proportion).sample(frac=1)
+    test_patches = list(set(unique_patches) - set(train_patches))
+    test_data = features.loc[features.index.get_level_values('patch_id').isin(test_patches)]
 
-    losses['model'] = str(Model)
-    losses['space'] = space
-    losses['features'] = features_file_name
-    losses['num_batches_train'] = num_batches
-    losses['num_batches_test'] = num_batches_test
-    identifier = round(time())
-    with open('{}/experiment_{}.json'.format(RESULTS_PATH, identifier), 'w') as f:
-        json.dump(str(losses), f)
-    if 'val_recall_positives' in losses.keys():
-        if losses['val_recall_positives'][-1] > 0.4 and losses['val_precision_positives'][-1] > 0.1:
-            model.save('logs/models/model_{}.h5'.format(identifier))
+    train_indices = data_stream._get_index_generator(train_data_upsampled, batch_size)
+    test_indices = data_stream._get_index_generator(test_data, test_batch_size)
+    train_generator = data_stream.get_train_data_generator_from_index(
+        data=[train_data_upsampled['image'], train_data_upsampled['destroyed']],
+        index=train_indices,
+        augment=True
+    )
+    test_generator = data_stream.get_train_data_generator_from_index(
+        data=[test_data['image'], test_data['destroyed']],
+        index=test_indices
+    )
+    train_dataset = Dataset.from_generator(lambda: train_generator, (tf.float32, tf.int32))
+    test_dataset = Dataset.from_generator(lambda: test_generator, (tf.float32, tf.int32))
+
+    num_batches = len(train_indices)
+    num_batches_test = len(test_indices)
+#Validate
+    for space in spaces:
+        space['batch_size'] = batch_size
+        space['prop_1_to_0'] = class_proportion[1]
+        space['class_weight'] = {
+            0: 1,#class_proportion[1],
+            1: 1# - class_proportion[1]
+        }
+        print('Now validating:\n')
+        print(space)
+        try:
+            model = Model(**space)
+            losses = model.validate_generator(train_dataset, test_dataset,
+                                              steps_per_epoch=num_batches,
+                                              validation_steps=num_batches_test,
+                                              **space)
+        except Exception as e:
+            losses = {'log': str(e)}
+
+        losses['model'] = str(Model)
+        losses['space'] = space
+        losses['features'] = features_file_name
+        losses['num_batches_train'] = num_batches
+        losses['num_batches_test'] = num_batches_test
+        identifier = round(time())
+        with open('{}/experiment_{}.json'.format(RESULTS_PATH, identifier), 'w') as f:
+            json.dump(str(losses), f)
+        if 'val_recall_positives' in losses.keys():
+            if losses['val_recall_positives'][-1] > 0.4 and losses['val_precision_positives'][-1] > 0.1:
+                model.save('logs/models/model_{}.h5'.format(identifier))
