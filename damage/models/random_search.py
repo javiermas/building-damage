dddc8a669364f0b1368fc6cd92bbc13c8e5c4e38
Javier Mas <javier.mas93@gmail.com>
2019-08-29 15:55:40 +0200

    :sparkles: Add sigmoid activation layer

--- damage/models/random_search.py
+++ damage/models/random_search.py
@@ -19,7 +19,7 @@ class RandomSearch:
         kernel_size = random.choice([3, 5, 7, 9])
         pool_size = kernel_size-1
         dropout = random.choice(np.linspace(0.1, 0.8, 10))
-        activation = random.choice(['relu']) # ReLU has shown much better performance
+        activation = random.choice(['sigmoid']) # ReLU has shown much better performance
         for _ in range(num_layers):
             filters = filters*2
             layer = {
@@ -35,13 +35,13 @@ class RandomSearch:
 
     @staticmethod
     def _sample_single_cnn_space():
-        num_layers = random.choice(range(2, 7))
+        num_layers = random.choice(range(2, 6))
         convolutional_layers = RandomSearch._sample_convolutional_layers(num_layers)
         total_filters = sum(layer['filters'] for layer in convolutional_layers)
         while total_filters > 1800:
             convolutional_layers = RandomSearch._sample_convolutional_layers(num_layers)
             total_filters = sum(layer['filters'] for layer in convolutional_layers)
-        
+
         space = {
             'dense_units': random.choice([16, 32, 64, 128, 256]),
             'batch_size': random.choice(range(25, 50)),
