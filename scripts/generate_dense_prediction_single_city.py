459a5c50e9074575ddb020d056004aad79446560
Javier Mas <javier.mas@barcelonagse.eu>
2019-11-25 21:46:54 +0100

    :lipstick: Rename predict_all to generate_dense_prediction_single_city

---
+++ scripts/generate_dense_prediction_single_city.py
@@ -0,0 +1,57 @@
+import os
+from math import ceil
+from time import time
+import argparse
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.data import Dataset
+from tensorflow.keras.models import load_model
+from sklearn.model_selection import KFold
+
+from damage.models import CNN, RandomSearch
+from damage.data import DataStream, load_experiment_results
+from damage import features
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--features', required=True)
+parser.add_argument('--gpu')
+parser.add_argument('--experiment')
+args = vars(parser.parse_args())
+
+os.environ['CUDA_VISIBLE_DEVICES'] = args.get('gpu', None) or '5'
+RESULTS_PATH = 'logs/predictions'
+FEATURES_PATH = 'logs/features'
+features_file_name = args.get('features')
+
+# Reading
+features = pd.read_pickle('{}/{}'.format(FEATURES_PATH, features_file_name))\
+    .dropna(subset=['image'])
+# features_destroyed = features.loc[features['destroyed'] == 1].sample(500)
+# features_non_destroyed = features.loc[features['destroyed'] == 0].sample(5000)
+# features = pd.concat([features_destroyed, features_non_destroyed])
+#  Modelling
+Model = CNN
+
+#  Choose Model
+model = load_model('logs/models/model_{}.h5'.format(args['experiment']))
+experiments = load_experiment_results()
+space = experiments.loc[
+    experiments['name'] == 'experiment_{}.json'.format(args['experiment']),
+    'space'
+].iloc[0]
+test_generator = DataStream._get_index_generator(features, space['batch_size'], KFold)
+num_batches_test = len(test_generator)
+test_generator = DataStream.get_test_data_generator_from_index(features['image'], test_generator)
+test_dataset = Dataset.from_generator(lambda: test_generator, tf.float32)
+
+#  Predict
+print('Generating predictions')
+predictions = model.predict_generator(test_dataset, steps=num_batches_test)
+predictions = pd.DataFrame({
+    'prediction': predictions.reshape(-1),
+}, index=features.index)
+file_name = '{}/prediction_{}.p'.format(RESULTS_PATH, round(time()))
+predictions.to_pickle(file_name)
+print('Store predictions on file: {}'.format(file_name))
