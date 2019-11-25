4548d468a6c7f9da3bb0a09c7615f6d24942c472
Javier Mas <javier.mas@barcelonagse.eu>
2019-11-25 21:42:26 +0100

    :sparkles: Add flag --wait-for-aleppo

--- scripts/compute_features_all_cities.sh
+++ scripts/compute_features_all_cities.sh
@@ -1,5 +1,18 @@
+#!/bin/bash 
+if [ "$1" == "--wait-for-aleppo" ]
+then
+    WAIT_FOR_ALEPPO=true
+else
+    WAIT_FOR_ALEPPO=false
+fi
+
 CITIES=('aleppo' 'idlib' 'damascus' 'homs' 'hama' 'daraa' 'raqqa' 'deir-ez-zor')
 for city in ${CITIES[@]}
 do
-    nohup python scripts/compute_features.py --city=$city --filename=${city}.p &
+    if [[ ($city == 'aleppo') && ($WAIT_FOR_ALEPPO = true) ]]
+    then
+        nohup python scripts/compute_features.py --city=$city --filename=${city}.p > "features_${city}.out"
+    else
+        nohup python scripts/compute_features.py --city=$city --filename=${city}.p > "features_${city}.out" &
+    fi
 done
