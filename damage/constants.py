27fc63b6bec611c1e0a87fad58546441329cedb5
Javier Mas <javier.mas@barcelonagse.eu>
2019-12-01 11:22:08 +0100

    :sparkles: Create file with constants

---
+++ damage/constants.py
@@ -0,0 +1,7 @@
+FEATURES_PATH='../../../../data/building_damage/features'
+EXPERIMENTS_PATH='../../../../data-old/building_damage/experiments'
+PREDICTIONS_PATH='../../../../data-old/building_damage/predictions'
+MODELS_PATH='../../../../data-old/building_damage/models'
+RASTERS_PATH='../../../../data/building_damage/city_rasters'
+ANNOTATIONS_PATH='../../../../data/building_damage/annotations'
+POLYGONS_PATH='../../../../data/building_damage/polygons'
